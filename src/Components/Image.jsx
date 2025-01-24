150ad9f786c510b22895aafa74feeb74c1035fd0
somonor011 <somonorhong011@gmail.com>
2025-01-24 22:05:00 +0700

    21. Local Images

--- src/Components/Image.jsx
+++ src/Components/Image.jsx
@@ -1,9 +1,8 @@
+import myPic from "../assets/img.png"
 
 const Image = () => {
     return <>
-        <img src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h
-            =750&dpr=1" alt="My Pic"
-        />
+        <img src={myPic} alt="My Pic" />
     </>
 }
 
