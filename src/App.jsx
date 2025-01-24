4188e96c50ceb3f96dfdb595b05db461d637bb97
somonor011 <somonorhong011@gmail.com>
2025-01-24 21:14:03 +0700

    13. Nested Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 
 
 const App = () => {
-    return (
-        <>
-            <h1>Hello world</h1>
-            <p>Hi</p>
-        </>
-    );
+    return <>
+        <Greeting/>
+        <Job/>
+        <Message/>
+    </>;
+}
+
+function Greeting (){
+    return <h2>Hello</h2>
+}
+
+function Job (){
+    return <h3>I am a teacher</h3>
+}
+
+function Message() {
+    return <h4>Lorem ipsum dolor sit amet, consetetur sadipsc</h4>
 }
 
 export default App;
